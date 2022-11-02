58afb6ba39fd3feb8e27b2855ec8076fcfaf226e
unknown <omoeradavid@gmail.com>
2022-11-02 22:13:23 +0100

    (rust practice week 3)

---
+++ week-3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\Davin\Desktop\A.nwaforcsc101\week-3\practice_2\target\debug\practice_2.exe: C:\Users\Davin\Desktop\A.nwaforcsc101\week-3\practice_2\src\main.rs
---
+++ week-3/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\Davin\Desktop\A.nwaforcsc101\week-3\practice_8\target\debug\practice_8.exe: C:\Users\Davin\Desktop\A.nwaforcsc101\week-3\practice_8\src\main.rs
